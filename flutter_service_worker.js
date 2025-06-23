194b853c65925f815fb870c72e0a3f1bdfc17581
Aubrey Barnard <aubreybar2@gmail.com>
2025-06-23 20:39:28 +0200

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -47,7 +47,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "817220cc15d56437e65ea4ba98f7ad08
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "e2eddba074f576c37b14f6da5c347524",
+"flutter_bootstrap.js": "413a2ed6e364afcae38d98b19b1135b6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
